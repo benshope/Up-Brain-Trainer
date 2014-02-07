ea4af38937076304b4f50d75af0482c5a1e7cea7
Ben Shope <ben@benshope.com>
2014-02-06 17:41:39 -0800

    Attempt to get Blocks Working

--- www/javascripts/application.js
+++ www/javascripts/application.js
@@ -1,31 +1,182 @@
 function ZenCtrl($scope) {
-  $scope.todos = [
-    {text:'learn angular', done:true},
-    {text:'build an angular app', done:false}];
- 
-  $scope.addTodo = function() {
-    $scope.todos.push({text:$scope.todoText, done:false});
-    $scope.todoText = '';
-  };
- 
-  $scope.remaining = function() {
-    var count = 0;
-    angular.forEach($scope.todos, function(todo) {
-      count += todo.done ? 0 : 1;
-    });
-    return count;
-  };
- 
-  $scope.archive = function() {
-    var oldTodos = $scope.todos;
-    $scope.todos = [];
-    angular.forEach(oldTodos, function(todo) {
-      if (!todo.done) $scope.todos.push(todo);
-    });
-  };
+    $scope.num_cells = 3;
+    $scope.cells = ["","","",];
+
+    $scope.getcells = function() {
+        $scope.cells = ["",""];
+        for(var i = 0; i < $scope.num_cells; i++) {
+            $scope.cells.push("");
+        }
+        return $scope.$apply();
+    };
+
+
+  //   $scope.board_side = 1;
+  //   $scope.board_size = function() {
+  //       return $scope.board_side^2;
+  //   };
+
+  //   $scope.visual_items = [1, 3, 5, 7];
+  //   $scope.audio_items = [0, 1, 2, 3];
+  //   $scope.difficulty = 2;
+  //   $scope.difficulty_options = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+  //   $scope.cell_status = function(i) {
+  //       if (i === $scope.visual_item) { return "active"; } 
+  //       else { return "inactive"; }
+  //   };
+
+  //   $scope.stop = function() {
+  //       $scope.tries = 0;
+  //       $scope.success = 0;
+  //       $scope.accuracy = 0;
+
+  //       $scope.false_positives = 0;
+  //       $scope.false_negatives = 0;
+  //       $scope.visual_item = null;
+  //       $scope.visual_queue = [];
+  //       $scope.audio_item = null;
+  //       $scope.audio_queue = [];
+  //       $scope.status = "paused";
+  //       $scope.shortcuts = [
+  //       {
+  //           key: 'enter',
+  //           description: 'begin'
+  //       }
+  //       ];
+  //       $scope.visual_item = null;
+  //       return $scope.$apply();
+  //   };
+
+
+  // $scope.start = function() {
+  //   console.log("starting");
+  //   $scope.status = "running";
+  //   $scope.shortcuts = [
+  //     {
+  //       key: 'enter',
+  //       description: 'stop'
+  //     }, {
+  //       key: 'up',
+  //       description: 'no match'
+  //     }, {
+  //       key: 'left',
+  //       description: 'visual match'
+  //     }, {
+  //       key: 'right',
+  //       description: 'sound match'
+  //     }, {
+  //       key: 'down',
+  //       description: 'visual+sound match'
+  //     }
+  //   ];
+  //   return $scope.next();
+  // };
+
+  // $scope.next = function() {
+  //   console.log("next");
+  //   if ($scope.visual_item !== null) {
+  //     $scope.visual_queue.unshift($scope.visual_item);
+  //   }
+  //   $scope.visual_item = $scope.visual_items[Math.floor(Math.random() * $scope.visual_items.length)];
+  //   if ($scope.audio_item !== null) {
+  //     $scope.audio_queue.unshift($scope.audio_item);
+  //   }
+  //   $scope.audio_item = $scope.audio_items[Math.floor(Math.random() * $scope.audio_items.length)];
+  //   document.getElementById("audio-" + $scope.audio_item).play();
+  //   return $scope.$apply();
+  // };
+
+
+  // $scope.evaluate = function(visual, audio) {
+  //   var audio_answer, success, visual_answer;
+  //   console.log("GUESS: " + visual + ", " + audio);
+  //   $scope.tries += 1;
+  //   success = false;
+  //   if ($scope.visual_queue.length < $scope.difficulty) {
+  //     if (!visual) {
+  //       success = true;
+  //     } else {
+  //       $scope.false_positives += 1;
+  //     }
+  //   } else {
+  //     visual_answer = $scope.visual_queue.pop() === $scope.visual_item;
+  //     audio_answer = $scope.audio_queue.pop() === $scope.audio_item;
+  //     if (visual === visual_answer && audio === audio_answer) {
+  //       success = true;
+  //     }
+  //     if (visual_answer && !visual) {
+  //       $scope.false_negatives += 1;
+  //     }
+  //     if (audio_answer && !audio) {
+  //       $scope.false_negatives += 1;
+  //     }
+  //     if (!visual_answer && visual) {
+  //       $scope.false_positives += 1;
+  //     }
+  //     if (!audio_answer && audio) {
+  //       $scope.false_positives += 1;
+  //     }
+  //   }
+  //   if (success) {
+  //     $scope.success += 1;
+  //   }
+  //   $scope.accuracy = Math.round(100.0 * $scope.success / $scope.tries);
+  //   return $scope.next();
+  // };
+  // Mousetrap.bind('up', function(e) {
+  //   if ($scope.status === 'running') {
+  //     return $scope.evaluate(false, false);
+  //   }
+  // });
+  // Mousetrap.bind('left', function(e) {
+  //   if ($scope.status === 'running') {
+  //     return $scope.evaluate(true, false);
+  //   }
+  // });
+  // Mousetrap.bind('right', function(e) {
+  //   if ($scope.status === 'running') {
+  //     return $scope.evaluate(false, true);
+  //   }
+  // });
+  // Mousetrap.bind('down', function(e) {
+  //   if ($scope.status === 'running') {
+  //     return $scope.evaluate(true, true);
+  //   }
+  // });
+  // return Mousetrap.bind('enter', function(e) {
+  //   if ($scope.status === "paused") {
+  //     return $scope.start();
+  //   } else {
+  //     return $scope.stop();
+  //   }
+  // });
+
+    // $scope.todos = [
+    // {text:'learn angular', done:true},
+    // {text:'build an angular app', done:false} ];
+
+    // $scope.addTodo = function() {
+    //     $scope.todos.push({ text:$scope.todoText, done:false });
+    //     $scope.todoText = '';
+    // };
+
+    // $scope.remaining = function() {
+    //     var count = 0;
+    //     angular.forEach($scope.todos, function(todo) {
+    //       count += todo.done ? 0 : 1;
+    // });
+    //     return count;
+    // };
+
+    // $scope.archive = function() {
+    //     var oldTodos = $scope.todos;
+    //     $scope.todos = [];
+    //     angular.forEach(oldTodos, function(todo) {
+    //         if (!todo.done) $scope.todos.push(todo);
+    //     });
+    // };
 }
-
-
 
 
 
